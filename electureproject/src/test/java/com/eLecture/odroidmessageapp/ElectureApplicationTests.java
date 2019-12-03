aef7067ac5f56c60b656de0026343bbaae2fc44c
MoritzderProgrammierer <51077669+MoritzderProgrammierer@users.noreply.github.com>
2019-12-03 11:27:23 +0100

    SpringBoot Projekt

---
+++ electureproject/src/test/java/com/eLecture/odroidmessageapp/ElectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eLecture.odroidmessageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
