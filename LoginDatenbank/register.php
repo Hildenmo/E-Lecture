023bb7770481505661d20a2ad5255151e4860d3a
MoritzderProgrammierer <51077669+MoritzderProgrammierer@users.noreply.github.com>
2019-12-12 12:45:03 +0100

    Um Nutzer in Datenbank anzulegen

---
+++ LoginDatenbank/register.php
@@ -0,0 +1,109 @@
+<?php 
+session_start();
+require_once('konfiguration.php');
+?>
+<!DOCTYPE html> 
+<html> 
+<head>
+  <title>Registrierung</title>    
+</head> 
+<body>
+ 
+<?php
+
+$showFormular = true; //Variable ob das Registrierungsformular anezeigt werden soll
+
+if(isset($_GET['register'])) {
+    $error = false;
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $passwort = $_POST['passwort'];
+    $passwort2 = $_POST['passwort2'];
+  
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
+        $error = true;
+    }     
+    if(strlen($passwort) == 0) {
+        echo 'Bitte ein Passwort angeben<br>';
+        $error = true;
+    }
+    if($passwort != $passwort2) {
+        echo 'Die Passwörter müssen übereinstimmen<br>';
+        $error = true;
+    }
+    
+    //Überprüfe, dass der Benutzername noch nicht vergeben wurde
+
+    if(!$error) {
+        $statement = $pdo->prepare("SELECT * FROM nutzer WHERE Benutzername = :username");
+        $result = $statement->execute(array('username' => $username));
+        $user = $statement->fetch();
+
+        if($user !== false) {
+            echo 'Dieser Benutzername ist bereits vergeben<br>';
+            $error = true;
+        }
+
+    }
+    //Überprüfe, dass die E-Mail-Adresse noch nicht registriert wurde
+    if(!$error) { 
+        $statement = $pdo->prepare("SELECT * FROM nutzer WHERE email = :email");
+        $result = $statement->execute(array('email' => $email));
+        $user = $statement->fetch();
+        
+        if($user !== false) {
+            echo 'Diese E-Mail-Adresse ist bereits vergeben<br>';
+            $error = true;
+        }    
+    }
+
+    
+    
+    //Keine Fehler, wir können den Nutzer registrieren
+    if(!$error) {    
+        $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
+        
+        $statement = $pdo->prepare("INSERT INTO nutzer (Benutzername, Email, Passwort) VALUES (:username, :email, :passwort)");
+        $result = $statement->execute(array('username' => $username, 'email' => $email, 'passwort' => $passwort_hash));
+        
+        if($result) {        
+            echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
+            $showFormular = false;
+        } else {
+            echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+        }
+    } 
+}
+ 
+if($showFormular) {
+?>
+ 
+<form action="?register=1" method="post">
+
+Nutzername:<br>
+<input type="text" size="40" maxlength="250" name="username"><br><br>
+
+
+E-Mail:<br>
+<input type="email" size="40" maxlength="250" name="email"><br><br>
+ 
+Dein Passwort:<br>
+<input type="password" size="40"  maxlength="250" name="passwort"><br>
+ 
+Passwort wiederholen:<br>
+<input type="password" size="40" maxlength="250" name="passwort2"><br><br>
+ 
+<input type="submit" value="Abschicken">
+</form>
+
+<a href="login.php">Zum Login</a>
+ 
+<a href="changepassword.php">Passwort ändern</a>
+
+<?php
+} //Ende von if($showFormular)
+?>
+ 
+</body>
+</html>
