4a030e0fd075001a00ad9ab273bb65fa161ac40d
Hildenmo <33152741+Hildenmo@users.noreply.github.com>
2019-12-13 00:04:15 +0100

    verbesserung der Registierung

--- register.php
+++ register.php
@@ -5,8 +5,20 @@ require_once('konfiguration.php');
 <!DOCTYPE html> 
 <html> 
 <head>
-  <title>Registrierung</title>    
-</head> 
+    <title>E-Lecture Login</title>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="template.css">  
+</head>
 <body>
  
 <?php
@@ -41,8 +53,21 @@ if(isset($_GET['register'])) {
         $user = $statement->fetch();
 
         if($user !== false) {
-            echo 'Dieser Benutzername ist bereits vergeben<br>';
+			
+							?>
+		<div class="container-fluid text-center loginscreen">
+    <br>
+    <img title="" src="https://cdn4.iconfinder.com/data/icons/e-learning-2-4/66/109-512.png" alt="" height="100" />
+    <h1 style="display:inline; color: black"><b>E-Lecture</b></h1>
+		</b> <h4> Dieser Benutzername ist bereits vergeben </h4>
+		<b> <a href="register.php">Zur Registierung</a>
+			
+			 </div>
+			
+			<?php
+		
             $error = true;
+			$showFormular = false;
         }
 
     }
@@ -53,8 +78,22 @@ if(isset($_GET['register'])) {
         $user = $statement->fetch();
         
         if($user !== false) {
-            echo 'Diese E-Mail-Adresse ist bereits vergeben<br>';
+			
+					?>
+		<div class="container-fluid text-center loginscreen">
+    <br>
+    <img title="" src="https://cdn4.iconfinder.com/data/icons/e-learning-2-4/66/109-512.png" alt="" height="100" />
+    <h1 style="display:inline; color: black"><b>E-Lecture</b></h1>
+		</b> <h4> Diese E-Mail-Adresse ist bereits vergeben </h4>
+		<b> <a href="register.php">Zur Registierung</a>
+			
+			 </div>
+			
+			<?php
+    
+			
             $error = true;
+			$showFormular = false;
         }    
     }
 
@@ -67,11 +106,35 @@ if(isset($_GET['register'])) {
         $statement = $pdo->prepare("INSERT INTO nutzer (benutzername, passwort, email) VALUES (:username, :passwort, :email)");
         $result = $statement->execute(array('username' => $username, 'passwort' => $passwort_hash, 'email' => $email));
         
-        if($result) {        
-            echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
+        if($result) {   
+			
+			?>
+		<div class="container-fluid text-center loginscreen">
+    <br>
+    <img title="" src="https://cdn4.iconfinder.com/data/icons/e-learning-2-4/66/109-512.png" alt="" height="100" />
+    <h1 style="display:inline; color: black"><b>E-Lecture</b></h1>
+		</b> <h4> Du wurdest erfolgreich registriert. </h4>
+		<b> <a href="login.php">Zum Login</a>
+			
+			 </div>
+			
+			<?php
             $showFormular = false;
         } else {
-            echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+			
+				?>
+		<div class="container-fluid text-center loginscreen">
+    <br>
+    <img title="" src="https://cdn4.iconfinder.com/data/icons/e-learning-2-4/66/109-512.png" alt="" height="100" />
+    <h1 style="display:inline; color: black"><b>E-Lecture</b></h1>
+		</b> <h4> Beim Abspeichern ist leider ein Fehler aufgetreten </h4>
+		<b> <a href="register.php">Zur Registierung</a>
+			
+			 </div>
+			
+			<?php
+          
+			
         }
     } 
 }
@@ -79,27 +142,61 @@ if(isset($_GET['register'])) {
 if($showFormular) {
 ?>
  
-<form action="?register=1" method="post">
+ <div id="register">
+  <div class="container-fluid text-center loginscreen">
+    <br>
+    <img title="" src="https://cdn4.iconfinder.com/data/icons/e-learning-2-4/66/109-512.png" alt="" height="100" />
+    <h1 style="display:inline; color: black"><b>E-Lecture</b></h1>
+      <div class="register">
+        <div class="heading">
+          <br>
+          <h2 style="color: black">Registierungsformular</h2>
+          
+          <?php 
+          if(isset($errorMessage)) {
+          echo $errorMessage;
+          }
+          ?>
+          <br>
+            <form action="?register=1" method="post">
+			<h4>Nutzername:</h4>
+              <div class="input-group input-group-lg">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+				
+                <input type="text" size="40" maxlength="250" name="username" class="form-control" placeholder="Benutzername">
+              </div>
+			  <h4>E-mail:</h4>
+			  <div class="input-group input-group-lg">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+				
+                <input type="email" size="40" maxlength="250" name="email" class="form-control" placeholder="E-Mail">
+              </div>
+			  <h4>Dein Passwort:</h4>
+			  <div class="input-group input-group-lg">
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+				
+                <input type="password" size="40" maxlength="250" name="passwort" class="form-control" placeholder="Passwort">
+              </div>
+			  
+			  <h4>Passwort wiederholen:</h4>
+              <div class="input-group input-group-lg">
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+				
+                <input type="password" size="40"  maxlength="250" name="passwort2" class="form-control" placeholder="Passwort wiederholen"><br><br>
+              </div>
+        </div>
+		<br>
+        <button type="submit" class="float" margin: 25px>Abschicken</button>
+		<br><a href="login.php">Zum Login</a>
+        </form>
+      </div>
+  </div>
+</div>
 
-Nutzername:<br>
-<input type="text" size="40" maxlength="250" name="username"><br><br>
+ 
+<!--   <a href="changepassword.php">Passwort ändern</a> -->
+ 
 
-
-E-Mail:<br>
-<input type="email" size="40" maxlength="250" name="email"><br><br>
- 
-Dein Passwort:<br>
-<input type="password" size="40"  maxlength="250" name="passwort"><br>
- 
-Passwort wiederholen:<br>
-<input type="password" size="40" maxlength="250" name="passwort2"><br><br>
- 
-<input type="submit" value="Abschicken">
-</form>
-
-<a href="login.php">Zum Login</a>
- 
-<a href="changepassword.php">Passwort ändern</a>
 
 <?php
 } //Ende von if($showFormular)
